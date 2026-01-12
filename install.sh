5fbb1316d72642a1758273f7d5bb01ce5f03d738
firewallfalcons <00xmahmoud@gmail.com>
2026-01-12 18:19:39 +0200

    Update install script for IPv4 and execution method

--- install.sh
+++ install.sh
@@ -7,11 +7,16 @@ fi
 
 echo "Installing FirewallFalcon Manager..."
 
+# Force IPv4 to avoid GitHub IPv6 timeouts
 MENU_URL="https://raw.githubusercontent.com/firewallfalcons/FirewallFalcon-Manager/main/menu.sh"
 
-wget -q -O /usr/local/bin/menu "$MENU_URL"
+# Download using wget -4 (IPv4 only)
+wget -4 -q -O /usr/local/bin/menu "$MENU_URL"
+
+# Make executable
 chmod +x /usr/local/bin/menu
 
-/usr/local/bin/menu --install-setup
+# Execute using bash to bypass CRLF/Shebang issues
+bash /usr/local/bin/menu --install-setup
 
 echo "Installation complete! Type 'menu' to start."
